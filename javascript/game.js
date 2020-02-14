d07bb4b6e1eda75aa9445c88e6eb5d1d734a4de4
rachael gladwin <raej1428@gmail.com>
2020-02-14 14:02:33 -0700

    working on javascript else and null errors in wrongLetters variable.

--- javascript/game.js
+++ javascript/game.js
@@ -1,54 +1,55 @@
-window.onload = function()
-{
+window.onload = function () {
 
-var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-var answer = [];
-var guess = []; //user guess
-var letters = []; //correctly guessed letters
-var wrongLetters = []; //incorrectly guessed letters
-var counter = 0; //counts correct letters
-var guesses = 13; //counts users guesses
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    var answer = [];
+    var guess = []; //user guess
+    var letters = []; //correctly guessed letters
+    var wrongLetters = []; //incorrectly guessed letters
+    var counter = 0; //counts correct letters
+    var guesses = 13; //counts users guesses
 
-var wordList = ['Simba', 'Mickey', 'Goofy"', 'Woody', 'Stitch']; //FILL LIST LATER!!
+    var wordList = ["Simba", "Mickey", "Goofy", "Woody", "Stitch"]; //FILL LIST LATER!!
 
-//randomly chooses a word from wordList
-var word = wordList[Math.floor(Math.random() * wordList.length)];
-var guess = document.getElementById("guess");
-var answer = document.getElementById("answer");
-var wrongLetters = document.getElementById("wrongLetters");
+    //randomly chooses a word from wordList
+    var word = wordList[Math.floor(Math.random() * wordList.length)];
+    var guess = document.getElementById("guess");
+    var answer = document.getElementById("answer");
+    var wrongLetters = document.getElementById("wrongLetters");
 
-//choosen word is replaced with
-function start() {
-    for (i = 0; i < word.length; i++) {
-        letters[i] = "__";
-    }
-    document.getElementById("answer").innerHTML = letters.join("");
-}
-
-function checkLetter() {
-    document.onkeyup = function (event) {
-        guess = event.key.toUpperCase();
-        var found = false; //Check for a boolean value
-
+    //choosen word is replaced with
+    function start() {
         for (i = 0; i < word.length; i++) {
+            letters[i] = "__";
+        }
+        document.getElementById("answer").innerHTML = letters.join(" ");
+    }
 
-            if (guess === word[i]) {
+    function checkLetter() {
+        document.onkeyup = function (event) {
+            guess = event.key.toUpperCase();
+            var found = false; //Check for a boolean value
+
+            for (i = 0; i < word.length; i++) {
+
+                if (guess === word[i]) {
+                    letters[i] = guess;
+                    found = true;
+                    document.getElementById("answer").innerHTML = letters.join("-");
+                }
+            }
+            //if found return statement to return the character
+            if (found) return;
+
+            //wrong letters setup
+            else (guess === word[i])
+            {
                 letters[i] = guess;
-                document.getElementById("answer").innerHTML = letters.join(" ");
-                found = true;
+                found = false;
+                document.getElementById("wrongLetters").innerHTML = wrongLetters.join(", ");
             }
         }
-        //if found return statement to return the character
-        if (found) return;
 
-        //wrong letters setup
-        else (guess === word[i])
-        {wrongLetters.indexOf(guess) < 1;
-            wrongLetters.push(guess)};
-            document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
-        }
     }
-start();
-checkLetter();
+    start();
+    checkLetter();
 }
-

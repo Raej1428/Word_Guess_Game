4cfea20526dd6a1d9b6ff0d6c9f1aeb0a5c251d9
rachael gladwin <raej1428@gmail.com>
2020-02-18 12:14:55 -0700

    wokring on the game function and the checkletter function as they only return the first letter. I am also trying to get the game centered. It is responsive and centered on mobile but not the desktop. A lot of head scratching happening.

--- javascript/game.js
+++ javascript/game.js
@@ -2,9 +2,8 @@
 var words = ["Simba", "Mickey", "Goofy", "Woody", "Elsa", "Baymax", "Gonzo"]
 
 //empty variables 
-var randomWord = [];
-var currentWord = [];
-var lettersOfWord = [];
+var randomWord = "";
+var lettersOfWord = []
 var blanks = 0;
 var blanksAndCorrect = [];
 var wrongGuess = [];
@@ -20,28 +19,27 @@ var guessesRemaining = 13;
 
 function Game() {
     //computer generates random word from words array
-    var randomWord = words[Math.floor(Math.random() * words.length)];
+    randomWord = words[Math.floor(Math.random() * words.length)];
 
     // split the individual word into separate arrays, and store in new array 
-    lettersOfWord = randomWord.split(" ");
+    lettersOfWord = randomWord.split("");
 
     //store length of word in blanks, for later use
     blanks = lettersOfWord.length;
 
     //creating a loop to generate "_" for each letter in array stored in blanks
     for (var i = 0; i < blanks; i++) {
-        blanksAndCorrect.push("_ ");
-    
-
-    // //not sure if I needed this line but it kept giving me errors so its commented out to troubleshoot.
-    // document.getElementById("currentWord").innerHTML = blanksAndCorrect.join("  ");
-
-    //testing in case it doesn't work
+        blanksAndCorrect.push("_");
+    }
+    //console logging 
     console.log(randomWord);
     console.log(lettersOfWord)
     console.log(blanks)
     console.log(blanksAndCorrect)
+    //showing the "_" within HTML
+    // document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
 }
+
 //Bonus Homework testing
 //variables for audio function
 var simba = document.getElementById("Simba");
@@ -139,33 +137,6 @@ function audio() {
         document.getElementById("image").src = "./assets/gonzo.png";
     }
 };
-
-//Check letters function
-function checkLetters(letter) {
-    var letterInWord = false;
-    //if the generated randomword is equal to the letter entered... then variable is true
-    for (var i = 0; i < blanks; i++) {
-        if (randomWord[i] == letter) {
-            letterInWord = true;
-        }
-    }
-    //if letterInWord (false)
-    if (letterInWord) {
-        //check each letter to see if it matches word
-        for (var i = 0; i < blanks; i++) {
-            if (randomWord[i] == letter) {
-                blanksAndCorrect[i] = letter;
-            }
-        }
-    }
-    //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
-    else {
-        wrongGuess.push(letter);
-        guessesRemaining--;
-    }
-    console.log(blanksAndCorrect);
-}
-
 //RESET FUNCTION
 function reset() {
     guessesRemaining = 13;
@@ -200,6 +171,31 @@ function checkLetters(letter) {
     console.log(blanksAndCorrect);
 }
 
+function complete() {
+    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
+
+    //if WON...then alert, play audio, display image and reset new round
+    if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
+        wins++;
+        aud()
+        reset()
+        //display wins on screen
+        document.getElementById("winstracker").innerHTML = " " + wins;
+
+        //if LOST...then alert and reset new round
+    } else if (guessesRemaining === 0) {
+        losses++;
+        reset()
+        document.getElementById("image").src = "./images/try-again.png"
+        document.getElementById("losstracker").innerHTML = " " + losses;
+    }
+    //display losses on screen && guesses remaining countdown
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join(" ");
+    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
+}
+//EXCUTING THE CODE
+
+
 //check for keyup, and convert to uppercase then store in guesses
 document.onkeyup = function (event) {
     var guesses = String.fromCharCode(event.keyCode).toUpperCase();
@@ -213,15 +209,7 @@ document.onkeyup = function (event) {
     //display/store incorrect letters on screen
     document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
 }
-}
-//EXCUTING THE CODE
+
+
+
 Game()
-
-
-
-
-
-
-
-
-
